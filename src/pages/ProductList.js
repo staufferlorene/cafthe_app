225afa82321a6a3d0f763978460adca264eef666
lorenestauffer <lorene.stauffer@campus-centre.fr>
2025-02-10 14:08:37 +0100

    Ajout squelette au chargement sur ProductList

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,9 +1,12 @@
-import React, {useEffect, useState} from 'react';
+import React, {use, useEffect, useState} from 'react';
 import axios from "axios";
 import ProductCard from "../components/ProductCard";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 function ProductList(props) {
     const [produits, setProduits] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchProduits = async () => {
@@ -12,11 +15,34 @@ function ProductList(props) {
                 setProduits(response.data);
             } catch (error){
                 console.error("Erreur de chargement des produits", error);
+            } finally {
+                setIsLoading(false); /* On arrête d'afficher le chargement (squelette) */
             }
         };
 
         void fetchProduits()
     }, []);
+
+    if (isLoading){
+        return (
+            <div className="product-list">
+                {Array.from({length : 6}).map((_,i) => (
+                    <div key={i} className="product-skeleton">
+                        {/*Image*/}
+                        <Skeleton height={200} width={300} />
+
+                        <div style={{marginTop: "10px"}}>
+                            <Skeleton height={20} width="70%" />
+                        </div>
+
+                        <div style={{marginTop: "10px"}}>
+                            <Skeleton height={20} width="40%" />
+                        </div>
+                    </div>
+                ))}
+            </div>
+        );
+    }
 
     return (
         <div>
