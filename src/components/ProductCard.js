799ced0c6962697f4e2e61b0e0428c1c3a0c5315
lorenestauffer <lorene.stauffer@campus-centre.fr>
2025-02-20 22:56:45 +0100

    modification route pour l'image

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -7,7 +7,7 @@ import CalculateTtc from "./CalculateTTC";
 function ProductCard({produit}) {
     return (
         <div className="product-card">
-            <img className="product-img" src={`/cafe/${produit.Chemin_img}`} alt="image de produit vendu par notre enseigne"/>
+            <img className="product-img" src={`/${produit.Chemin_img}`} alt="image de produit vendu par notre enseigne"/>
             <h3>{produit.Nom_produit}</h3>
             <p><CalculateTtc produit={produit} /></p>
                 <Link to={`/produit/${produit.Id_produit}`} className="details-btn">
